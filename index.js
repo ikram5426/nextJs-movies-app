d070ccdc2bc58c3706c990a953875a6cdca81699
IkramKhan <ikram5426@gmail.com>
2023-05-13 23:25:43 +0500

    Add: Dropdown menu for small screens

---
+++ index.js
@@ -0,0 +1,18 @@
+const nav=document.querySelector('.primary-navigation');
+
+const toggleButton = document.querySelector(".mobile-nav-toggle");
+
+toggleButton.addEventListener("click",()=>{
+const visible=nav.getAttribute('isVisible');
+if(visible==='false'){
+  nav.setAttribute('isVisible',true);
+  toggleButton.setAttribute('aria-expanded',true);
+}else{
+  nav.setAttribute("isVisible", false);
+  toggleButton.setAttribute("aria-expanded", false);
+
+}
+console.log(toggleButton.getAttribute('aria-expanded'));
+})
+
+
